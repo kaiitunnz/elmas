28f81959a1092ffca3c155a8b37e13e6bd7172fc
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-11-10 01:19:21 -0800

    Change benchmark naming

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-MICROBENCHMARKS="generative_agents guessing_game multiturn_long multiturn_short sharegpt"
-E2E_BENCHMARKS="gptswarm_mmlu guessing_game_e2e guessing_game_e2e_cot"
-E2E_BENCHMARK_SERVERS="no-apc apc mt-apc mt-apc-no-sched"
+STANDARD_WORKLOADS="generative_agents guessing_game multiturn_long multiturn_short sharegpt"
+MA_WORKLOADS="gptswarm_mmlu guessing_game_e2e guessing_game_e2e_cot"
+MA_WORKLOADS_SERVERS="no-apc apc mt-apc mt-apc-no-sched"
 
 ROOT_RESULT_DIR="$HOME/Workspace/Projects/elmas/results"
 
-# Microbenchmarking experiments
+# Experiments with standard workloads
 python -O benchmarks/runner.py \
-    --benchmarks $MICROBENCHMARKS \
-    --num-trials=5\
-    --result-dir "$ROOT_RESULT_DIR/microbenchmarks" \
+    --benchmarks $STANDARD_WORKLOADS \
+    --num-trials=5 \
+    --result-dir "$ROOT_RESULT_DIR/standard" \
     --clear-result-dir
 
-# End-to-end experiments
+# Experiments with large-scale multi-agent workloads
 python -O benchmarks/runner.py \
-    --benchmarks $E2E_BENCHMARKS \
-    --servers $E2E_BENCHMARK_SERVERS \
-    --num-trials=3\
-    --result-dir "$ROOT_RESULT_DIR/e2e" \
+    --benchmarks $MA_WORKLOADS \
+    --servers $MA_WORKLOADS_SERVERS \
+    --num-trials=3 \
+    --result-dir "$ROOT_RESULT_DIR/multi_agent" \
     --clear-result-dir
