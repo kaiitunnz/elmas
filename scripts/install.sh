63664f14035b0ae4f1fc4704af6cdf3dc90aa8f9
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-11-19 09:37:59 -0800

    Fix vLLM wheel version

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,8 @@ set -e
 # Install our version of vLLM
 git clone https://github.com/kaiitunnz/vllm.git
 cd vllm
-pip install https://vllm-wheels.s3.us-west-2.amazonaws.com/nightly/vllm-1.0.0.dev-cp38-abi3-manylinux1_x86_64.whl
+# Install the wheel for v0.6.3.post1
+pip install https://vllm-wheels.s3.us-west-2.amazonaws.com/a2c71c5405fdd8822956bcd785e72149c1cfb655/vllm-1.0.0.dev-cp38-abi3-manylinux1_x86_64.whl
 python python_only_dev.py
 cd -
 
